5011ffee4f36ce38cf9d183b310c17f1a0268305
mas212 <mas32957@gmail.com>
2020-03-27 16:19:13 +0700

    array sort

---
+++ sort.php
@@ -0,0 +1,11 @@
+<?php
+$numbers = [1, 4, 8, 12, 16, 20, 30];
+
+usort($numbers, function($first, $second){
+	if($first === $second){
+		return 0;
+	}
+	return ($first > $second) ? 1 : -1;
+});
+
+var_dump($numbers);
